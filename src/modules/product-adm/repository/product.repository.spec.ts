ee4bde20f10f1e6a569eb30875a941d5ae75427f
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-15 07:31:10 -0300

    Implementando create product

--- src/modules/product-adm/repository/product.repository.spec.ts
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "./product.model";
 import Product from "../domain/product.entity";
+import ProductRepository from "./product.repository";
+import Id from "../../@shared/domain/value-object/id.value-object";
 
 describe("ProductRepository test", () => {
   let sequelize: Sequelize;
@@ -19,5 +21,27 @@ describe("ProductRepository test", () => {
     await sequelize.close();
   });
 
-  it("should create a product", async () => {});
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product({
+      id: new Id("1"),
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    });
+    await productRepository.add(product);
+    const productModel = await ProductModel.findOne({
+      where: { id: product.id.id },
+    });
+    expect(productModel.toJSON()).toStrictEqual({
+      id: product.id.id,
+      name: product.name,
+      description: product.description,
+      purchasePrice: product.purchasePrice,
+      stock: product.stock,
+      createdAt: product.createdAt,
+      updatedAt: product.updatedAt,
+    });
+  });
 });
