c5ddda02c1af956dccd8c0d33296fb6635ba674e
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-15 08:56:45 -0300

    Implementando find a product

--- src/modules/product-adm/repository/product.repository.ts
+++ src/modules/product-adm/repository/product.repository.ts
@@ -1,3 +1,4 @@
+import Id from "../../@shared/domain/value-object/id.value-object";
 import Product from "../domain/product.entity";
 import ProductGateway from "../gateway/product.gateway";
 import { ProductModel } from "./product.model";
@@ -15,6 +16,18 @@ export default class ProductRepository implements ProductGateway {
     });
   }
   async find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+    const product = await ProductModel.findOne({ where: { id } });
+    if (!product) {
+      throw new Error(`Product with id ${id} not found`);
+    }
+    return new Product({
+      id: new Id(product.id),
+      name: product.name,
+      description: product.description,
+      purchasePrice: product.purchasePrice,
+      stock: product.stock,
+      createdAt: product.createdAt,
+      updatedAt: product.updatedAt,
+    });
   }
 }
