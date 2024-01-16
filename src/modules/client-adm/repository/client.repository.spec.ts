b46f958b2a8317d36b7ac2684f8d8321c2febb08
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-16 17:42:40 -0300

    Criando ClientModel

---
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize-typescript";
+import { ClientModel } from "./client.model";
+
+describe("ClientRepository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+    await sequelize.addModels([ClientModel]);
+    await sequelize.sync();
+  });
+  afterEach(async () => {
+    await sequelize.close();
+  });
+});
