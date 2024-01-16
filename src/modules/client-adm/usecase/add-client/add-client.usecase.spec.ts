55e7ab1d6df800b0e6be142690899999490b9e86
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-16 17:19:45 -0300

    Implementando case de uso AddClient

--- src/modules/client-adm/usecase/add-client/add-client.usecase.spec.ts
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.spec.ts
@@ -1,2 +1,28 @@
+import AddClientUseCase from "./add-client.usecase";
 
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn(),
+  };
+};
 
+describe("AddClientUsecase unit test", () => {
+  it("should add a client", async () => {
+    const clientRepository = MockRepository();
+    const useCase = new AddClientUseCase(clientRepository);
+
+    const input = {
+      name: "Client 1",
+      email: "email",
+      address: "address",
+    };
+
+    const result = await useCase.execute(input);
+    expect(clientRepository.add).toHaveBeenCalled();
+    expect(result.id).toBeDefined();
+    expect(result.name).toBe(input.name);
+    expect(result.email).toBe(input.email);
+    expect(result.address).toBe(input.address);
+  });
+});
