333cb30440513639f683cc1bfbe4385bc4aa894d
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-17 07:30:29 -0300

    Implementando Add no ClientRepository

--- src/modules/client-adm/repository/client.repository.spec.ts
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { ClientModel } from "./client.model";
 import ClientRepository from "./client.repository";
+import Client from "../domain/client.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
 
 describe("ClientRepository test", () => {
   let sequelize: Sequelize;
@@ -17,6 +19,33 @@ describe("ClientRepository test", () => {
   });
   afterEach(async () => {
     await sequelize.close();
+  });
+
+  it("should create a client", async () => {
+    const clientRepository = new ClientRepository();
+    const client = new Client({
+      id: new Id("1"),
+      name: "Client 1",
+      email: "email",
+      address: "address",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await clientRepository.add(client);
+
+    const clientDb = await ClientModel.findOne({
+      where: {
+        id: "1",
+      },
+    });
+    expect(clientDb).toBeDefined();
+    expect(clientDb.id).toEqual(client.id.id);
+    expect(clientDb.name).toEqual(client.name);
+    expect(clientDb.email).toEqual(client.email);
+    expect(clientDb.address).toEqual(client.address);
+    expect(clientDb.createdAt).toStrictEqual(client.createdAt);
+    expect(clientDb.updatedAt).toStrictEqual(client.updatedAt);
   });
 
   it("should find a client", async () => {
@@ -36,7 +65,7 @@ describe("ClientRepository test", () => {
     expect(result.name).toEqual(client.name);
     expect(result.email).toEqual(client.email);
     expect(result.address).toEqual(client.address);
-    expect(result.createdAt).toEqual(client.createdAt);
-    expect(result.updatedAt).toEqual(client.updatedAt);
+    expect(result.createdAt).toStrictEqual(client.createdAt);
+    expect(result.updatedAt).toStrictEqual(client.updatedAt);
   });
 });
