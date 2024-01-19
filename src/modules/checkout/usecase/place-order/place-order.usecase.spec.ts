f0e14d52eabf809c00a548fb8537206202aa5b8d
Paulo Sarmento <paulo.cesar.sarmento@hotmail.com>
2024-01-19 14:02:24 -0300

    implementando getProduct

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -1,6 +1,7 @@
 import PlaceOrderUseCase from "./place-order.usecase";
 import { PlaceOrderInputDto } from "./prace-order.dto";
 
+const mockDate = new Date(2000, 1, 1);
 describe("PlaceOrderUseCase unit test", () => {
   describe("ValidateProducts method", () => {
     //@ts-expect-error - no params in constructor
@@ -58,6 +59,31 @@ describe("PlaceOrderUseCase unit test", () => {
       ).rejects.toThrow("Product 1 is not available in stock");
 
       expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(5);
+    });
+  });
+
+  describe("GetProducts method", () => {
+    beforeAll(() => {
+      jest.useFakeTimers("modern");
+      jest.setSystemTime(mockDate);
+    });
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+    //@ts-expect-error - no params in constructor
+    const placeOrderUseCase = new PlaceOrderUseCase();
+
+    it("Should throw an error when product not found", async () => {
+      const mockCatalogFacade = {
+        find: jest.fn().mockResolvedValue(null),
+      };
+
+      //@ts-expect-error - force set catalogFacade
+      placeOrderUseCase["_catalogFacade"] = mockCatalogFacade;
+
+      await expect(placeOrderUseCase["getProducts"]("0")).rejects.toThrow(
+        "Product not found"
+      );
     });
   });
 
--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,17 +1,23 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import ClientAdmFacadeInterface from "../../../client-adm/facade/client-adm.facade.interface";
 import ProductAdmFacadeInterface from "../../../product-adm/facade/product-adm.facade.interface";
+import StoreCatalogFacadeInterface from "../../../store-catalog/facade/store-catalog.facade.interface";
+import Product from "../../domain/product.entity";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./prace-order.dto";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
   private _clientFacade: ClientAdmFacadeInterface;
   private _productFacade: ProductAdmFacadeInterface;
+  private _catalogFacade: StoreCatalogFacadeInterface;
   constructor(
     clientFacade: ClientAdmFacadeInterface,
-    productFacade: ProductAdmFacadeInterface
+    productFacade: ProductAdmFacadeInterface,
+    catalogFacade: StoreCatalogFacadeInterface
   ) {
     this._clientFacade = clientFacade;
     this._productFacade = productFacade;
+    this._catalogFacade = catalogFacade;
   }
 
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
@@ -54,5 +60,20 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
         throw new Error(`Product ${p.productId} is not available in stock`);
       }
     }
+  }
+
+  private async getProducts(productId: string): Promise<Product> {
+    const product = await this._catalogFacade.find({ id: productId });
+    if (!product) {
+      throw new Error("Product not found");
+    }
+    const productProps = {
+      id: new Id(product.id),
+      name: product.name,
+      description: product.description,
+      salesPrice: product.salesPrice,
+    };
+
+    return new Product(productProps);
   }
 }
